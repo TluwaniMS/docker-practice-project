c431511420866bebf810bad340e210632dfacf7a
tluwaniMS <tluwani2020@gmail.com>
2023-05-14 17:18:29 +0200

    adding-env-variables

--- app.js
+++ app.js
@@ -3,9 +3,16 @@ import morgan from 'morgan';
 
 export const app = express();
 
+const name = process.env.NAME || 'John'
+const surname = process.env.SURNAME || 'Doe'
+
+const message = `${name} ${surname} has accessed the end-point successfuly. :)`
+
 app.use(express.json());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 
 app.get('/api/access-end-point', (_, res) => {
+  console.log(message)
+  
   res.status(200).send({ message: 'The End-point has been accessed successfuly.' });
 });
